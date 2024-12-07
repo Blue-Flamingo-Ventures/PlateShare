76dbd471030f0dbf784bf6c6af2cfc52d270b36e
Julian Januszka <julianjanuszka@Julians-MacBook-Pro.local>
2024-12-07 12:15:55 -0500

    deeplink stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,22 @@ import React, { useState } from "react";
 import { View, Button, Text } from "react-native";
 import { Stack } from "expo-router";
 import CausalityClient from "../api/causality_client";
+import * as Linking from "expo-linking";
 
 export default function RootLayout() {
   const [result, setResult] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [status, setStatus] = useState<string | null>(null);
+  const [deepLink, setDeepLink] = useState<string | null>(null);
 
-  const handleTestCausalityClient = async () => {
-    const client = new CausalityClient({
-      "Content-Type": "application/json",
-    });
+  const client = new CausalityClient({
+    "Content-Type": "application/json",
+  });
 
+  const startRedeem = async () => {
     try {
-      setError(null);
       setResult(null);
-
       const response = await client.requestQrCode({
-        key: "$2y$10$W4dqnW2O.GzgpeVqYf2IieWM5rZSikke7WO9bM9wCV3sylVK9WoJ2",
-        token: "KchbMwUT",
+        key: "$2y$10$wjBFiFlS3medyxMYFw6JK.g2KBrv9oLKZ1aO0RMLoO8cdfrEyO5Ty",
+        token: "dhz7wo8J",
       });
 
       if (response.status === 200) {
@@ -28,27 +26,42 @@ export default function RootLayout() {
         setResult(
           `QR Code Link: ${response.qrCodeLink}\nQR Code: ${response.qrcode}\nDeeplink: ${response.deeplink}`
         );
+        setDeepLink(response.deeplink as string);
+        try {
+          const supported = await Linking.canOpenURL(deepLink as string);
+          if (supported) {
+            await Linking.openURL(deepLink as string);
+          } else {
+            console.error("Error", "App is not installed or deeplink is invalid.");
+          }
+        } catch (error) {
+          console.error("Error", ("Failed to open app: " + error) as string);
+        }
       } else {
         console.log(response.message);
-        setError(`Error: ${response.message}`);
+        setResult(null);
+        return;
       }
 
-      let qrCode = response.qrcode;
-
-      setTimeout(async (qrCode: string) => {
-        console.log(qrCode)
-        const response = await client.apiStatusCheck({
-          code: qrCode
-        })
-
-        setStatus(response.message + ' ' + response.status)
-      }, 5000, qrCode)
-
-      
+      const qrCode = response.qrcode;
+      for (let i = 5000; i < 30000; i + 5000) {
+        setTimeout(
+          async (qrCode: string, setter: any) => {
+            console.log(qrCode);
+            const response = await client.apiStatusCheck({
+              code: qrCode,
+            });
+            if (response.status == 200) {
+              console.log('response! + ', response.message as string)
+              setResult(response.product_name);
+            }
+          },
+          i,
+          qrCode,
+        );
+      }
     } catch (err: any) {
       console.log(err);
-
-      setError(`Failed to request QR code: ${err.message}`);
     }
   };
 
@@ -62,13 +75,8 @@ export default function RootLayout() {
           alignItems: "center",
         }}
       >
-        <Button
-          title="Test Causality"
-          onPress={handleTestCausalityClient}
-        />
+        <Button title="Test Causality" onPress={startRedeem} />
         {result && <Text>{result}</Text>}
-        {error && <Text style={{ color: "red" }}>{error}</Text>}
-        {status && <Text style={{ color: "red" }}>{status}</Text>}
       </View>
     </>
   );
